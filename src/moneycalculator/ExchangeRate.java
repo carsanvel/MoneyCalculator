869161cb12da6ec98be58d35ab02c32497f8b7ac
carvsk <carvsk@LAPTOP-IJ1BED1T>
2019-10-29 16:22:52 +0000

    MoneyCalculator 3 actualización

--- src/moneycalculator/ExchangeRate.java
+++ src/moneycalculator/ExchangeRate.java
@@ -1,14 +1,38 @@
 package moneycalculator;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 
 public class ExchangeRate {
 
-    private Currency from;
-    private Currency to;
-    private Date date;
-    private double rate;
+    private final Currency from;
+    private final Currency to;
+    private final LocalDate date;
+    private final double rate;
+
+    public ExchangeRate(Currency from, Currency to, LocalDate date, double rate) {
+        this.from = from;
+        this.to = to;
+        this.date = date;
+        this.rate = rate;
+    }
+
+    public Currency getFrom() {
+        return from;
+    }
+
+    public Currency getTo() {
+        return to;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getRate() {
+        return rate;
+    }
     
     
     
--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -1,5 +1,11 @@
 package moneycalculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -10,22 +16,22 @@ public class MoneyCalculator {
     
 
     private Map<String, Currency> currencies = new HashMap<String, Currency>();
-    private Currency currencyFrom;
     private Currency currencyTo;
     private Money money;
+    private ExchangeRate exchangeRate;
 
     public MoneyCalculator() {
         currencies.put("EUR", new Currency("EUR", "Euro", "€"));
         currencies.put("GBP", new Currency("USD", "Dollar americano", "$"));
         currencies.put("GBP", new Currency("GBP", "Libra", "£"));
-    }
+    }   
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         MoneyCalculator moneyCalculator = new MoneyCalculator();
         moneyCalculator.control();
     }
     
-    private void control() {
+    private void control() throws IOException{
         input();
         process();
         output();
@@ -36,21 +42,30 @@ public class MoneyCalculator {
         Scanner scanner = new Scanner(System.in);
         double amount = Double.parseDouble(scanner.next());
         System.out.println("Introduzca divisa de origen");
-        currencyFrom = currencies.get(scanner.next());
+        money = new Money(amount, currencies.get(scanner.next()));
         System.out.println("Introduzca divisa de destino");
         currencyTo = currencies.get(scanner.next());
     }
 
-    private void process() {
-        
+    private void process() throws IOException{
+        exchangeRate = new ExchangeRate(money.getCurrency(), currencyTo, LocalDate.MIN, getExchangeRate(money.getCurrency().getCode(), currencyTo.getCode()));
     }
 
     private void output() {
-        
+        System.out.println(money.getAmount() + " " + money.getCurrency().getSymbol() + " equivalen a: " +
+                            money.getAmount()*exchangeRate.getRate() +  currencyTo.getSymbol());
     }
     
-    private static double getExchangeRate(String from, String to) {
-        URL url = new URL("")
+    private static double getExchangeRate(String from, String to) throws IOException {
+        URL url = new URL("free.currconv.com/api/v7/convert?q=" + from + "_" + 
+                            to + "&compact=ultra&apiKey=610710caec3603c5f3c4");
+        URLConnection connection = url.openConnection();
+        try(BufferedReader reader = new BufferedReader
+                                    (new InputStreamReader(connection.getInputStream()))) {
+            String line = reader.readLine();
+            String line1 = line.substring(line.indexOf(to) + 12, line.indexOf("}"));
+            return Double.parseDouble(line1);
+        }
     }
     
 }
