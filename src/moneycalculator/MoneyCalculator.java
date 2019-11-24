68b05e64d688b0f61482e6d57aa2e4955e477ce1
carvsk <carvsk@LAPTOP-IJ1BED1T>
2019-11-24 18:18:43 +0000

    MoneyCalculator con interfaz

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -6,45 +6,15 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class MoneyCalculator {
-
-    
-    private final Map<String, Currency> currencies;
-    private Currency currencyTo;
-    private Money money;
-    private ExchangeRate exchangeRate;
-
-    public MoneyCalculator() throws IOException {
-        currencies = new HashMap<>();
-        FileCurrencyLoader.load(currencies, "C:\\Users\\carvsk\\Documents\\universidad\\Cuarto ano\\Primer cuatrimestre\\Ingeniería del software 2\\fichero divisas JSON2.txt");
-    }   
-    
-    public static void main(String[] args) throws IOException{
-        MoneyCalculator moneyCalculator = new MoneyCalculator();
-        moneyCalculator.control();
-    }
-    
-    private void control() throws IOException{
-        input();
-        process();
-        output();
-    }
         
-    private void input() {
-        System.out.println("Introduzca cantidad");
-        Scanner scanner = new Scanner(System.in);
-        double amount = Double.parseDouble(scanner.next());
-        System.out.println("Introduzca divisa de origen");
-        money = new Money(amount, currencies.get(scanner.next()));
-        System.out.println("Introduzca divisa de destino");
-        currencyTo = currencies.get(scanner.next());
+    public static void main(String[] args) throws IOException{
+        RestExchangeRateLoader exchangeRateLoader = new RestExchangeRateLoader("https://api.exchangeratesapi.io/latest?base=");
+        FileCurrencyLoader currencyLoader =  new FileCurrencyLoader("C:\\Users\\carvsk\\Documents\\universidad\\Cuarto ano\\Primer cuatrimestre\\Ingeniería del software 2\\fichero divisas JSON2.txt");
+        Map<String, Currency> currenciesList = new HashMap<>();
+        currencyLoader.load(currenciesList);
+        MainFrame mainFrame = new MainFrame(currenciesList);
+        mainFrame.add(new CalculateCommand(mainFrame.getMoneyDialog(), mainFrame.getMoneyDisplay(), (HashMap<String, Currency>) currenciesList, exchangeRateLoader));
+        mainFrame.add(new DeleteCommand(mainFrame.getMoneyDisplay()));
     }
-
-    private void process() throws IOException{
-        exchangeRate = RestExchangeRateLoader.load("https://api.exchangeratesapi.io/latest?base=", money.getCurrency(), currencyTo);
-    }
-
-    private void output() {
-        System.out.println(money.getAmount() + " " + money.getCurrency().getSymbol() + " equivalen a: " +
-                            money.getAmount()*exchangeRate.getRate() +  currencyTo.getSymbol());
-    }
+    
 }
